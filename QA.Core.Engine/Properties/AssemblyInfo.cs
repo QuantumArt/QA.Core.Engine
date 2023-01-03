c8b53f2b6aed2ad265adf4a83d7dcef978adb6d7
DESKTOP-TJDB0L9\PC <CelutP@gmail.com>
2023-01-03 19:13:48 +0300

    Update packages

--- QA.Core.Engine/Properties/AssemblyInfo.cs
+++ QA.Core.Engine/Properties/AssemblyInfo.cs
@@ -5,22 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("QA.Core.Engine")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QA.Core.Engine")]
-[assembly: AssemblyCopyright("Copyright © 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("2a96c6b9-e85a-4015-95e6-548144e8aa1d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,9 +19,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyInformationalVersion("2.2.0.0")]
+
 [assembly: InternalsVisibleTo("QA.Core.Engine.FakeImpl")]
 [assembly: InternalsVisibleTo("QA.Core.Engine.QPData")]
 [assembly: InternalsVisibleTo("QA.Core.Engine.Impl")]
